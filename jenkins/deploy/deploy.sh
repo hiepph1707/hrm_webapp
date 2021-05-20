6de7fd6e84d0a5377df2e1953d3c252543a90f31
hiepph1707 <57936136+hiepph1707@users.noreply.github.com>
2021-05-20 15:09:18 +0700

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DOCKER_REGISTRY="hub.saigonbpo.vn"
-docker login -u hiepph1707 -p $PASS
+docker login -u hiepph -p $PASS
 cd jenkins/deploy/ && /usr/local/bin/docker-compose -f docker-compose-deploy.yml up -d
